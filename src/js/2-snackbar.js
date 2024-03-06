9b0d03897542588b1ebeb2971d6742b59486b372
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2024-03-06 17:08:54 +0000

    update all

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,6 +1,45 @@
-import flatpickr from "flatpickr"
-import { Russian } from "flatpickr/dist/l10n/ru.js"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-flatpickr(myElem, {
-    "locale": Russian // locale for this instance only
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const delayInput = this.querySelector('[name="delay"]');
+    const stateInputs = this.querySelectorAll('[name="state"]');
+    const selectedState = [...stateInputs].find(input => input.checked);
+
+    if (!selectedState) {
+      return;
+    }
+
+    const delay = parseInt(delayInput.value, 10);
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (selectedState.value === "fulfilled") {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+
+    promise.then(
+      (value) => {
+        iziToast.success({
+          title: "Fulfilled promise",
+          message: `✅ Fulfilled promise in ${value}ms`,
+        });
+      },
+      (reason) => {
+        iziToast.error({
+          title: "Rejected promise",
+          message: `❌ Rejected promise in ${reason}ms`,
+        });
+      }
+    );
+  });
 });
